d0f3c7dae5690e438e6a45663d52d04ce2de1466
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2022-10-30 23:52:51 +0200

    2022-10-30

--- std_format.py
+++ std_format.py
@@ -2,8 +2,8 @@ import xlsxwriter
 
 # global variables
 XL = {}
-def writexl(loc, text):
-    worksheet.write(loc, text)
+def writexl(loc, text, format):
+    worksheet.write(loc, text, format)
     XL[loc] = text
 
 # READ CSV
@@ -17,22 +17,39 @@ workbook = xlsxwriter.Workbook('std.xlsx')
 worksheet = workbook.add_worksheet('טבלת דרישות')
 worksheet.right_to_left()
 
+# format
+whiteformat = workbook.add_format({'text_wrap': True, 'valign': 'vcenter', 'align': 'right', 'border': 1, 'bg_color': '#e1e8ed'})
+blackformat = workbook.add_format({'text_wrap': True, 'valign': 'vcenter', 'align': 'right', 'border': 1, 'bg_color': '#bcd3e3'})
+header_format = workbook.add_format({'bold': True, 'bg_color': 'black', 'color': 'white'})
+
+# cell size
+worksheet.set_column('A:A', 20)
+worksheet.set_column('B:B', 10)
+worksheet.set_column('C:C', 20)
+worksheet.set_column('D:D', 40)
+worksheet.set_column('E:E', 15)
+worksheet.set_column('F:F', 20)
+worksheet.set_column('G:G', 10)
+worksheet.set_column('H:H', 20)
+worksheet.set_column('I:I', 40)
+
 # write to XL
 # headers
-writexl("A1", "דרישת אב")
-writexl("B1", "מס דרישה")
-writexl("C1", "דרישת בן (פונקציה)")
-writexl("D1", "הסבר לפונקציה (מה)")
-writexl("E1", "סעיף תנאי הבדיקה")
-writexl("F1", "תנאי בדיקה test condition (איך)")
-writexl("G1", "מס תסריט")
-writexl("H1", "תסריט הבדיקה test case")
-writexl("I1", "מטרה")
+writexl("A1", "דרישת אב", header_format)
+writexl("B1", "מס דרישה", header_format)
+writexl("C1", "דרישת בן", header_format)
+writexl("D1", "הסבר לפונקציה", header_format)
+writexl("E1", "סעיף תנאי הבדיקה", header_format)
+writexl("F1", "תנאי בדיקה", header_format)
+writexl("G1", "מס תסריט", header_format)
+writexl("H1", "תסריט הבדיקה", header_format)
+writexl("I1", "מטרה", header_format)
 
 # READ LINES FROM CSV
 index = 1
 DRISHAT_BEN_ID = 0
 SEIF = 0
+color = 1
 for line in lines:
     index += 1
     # remove \n
@@ -72,16 +89,22 @@ for line in lines:
     else:
         write_SEIF = ""
 
-    # write
-    writexl("A"+str(index), line[0])
-    writexl("B"+str(index), write_DRISHAT_BEN_ID)
-    writexl("C"+str(index), line[1])
-    writexl("D"+str(index), line[2])
-    writexl("E"+str(index), write_SEIF)
-    writexl("F"+str(index), line[3])
-    writexl("G"+str(index), index-1)
-    writexl("H"+str(index), line[4])
-    writexl("I"+str(index), line[5])
+    # color
+    if line[0] != "":
+        color *= -1
+    if color == 1:
+        tf = whiteformat
+    else:
+        tf = blackformat
+    writexl("A"+str(index), line[0], tf)
+    writexl("B"+str(index), write_DRISHAT_BEN_ID, tf)
+    writexl("C"+str(index), line[1], tf)
+    writexl("D"+str(index), line[2], tf)
+    writexl("E"+str(index), write_SEIF, tf)
+    writexl("F"+str(index), line[3], tf)
+    writexl("G"+str(index), index-1, tf)
+    writexl("H"+str(index), line[4], tf)
+    writexl("I"+str(index), line[5], tf)
 
 # MERGE ver XL
 for area in "ABCDEFGHI":
